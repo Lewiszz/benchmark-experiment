7a9ec1560a14680cd9396008d6c2f5ffd8f61bff
Lewiszz <lewis@trueprice.org>
2023-07-14 14:28:47 +0200

    Very simple BenchmarkDotNet test works

--- Program.fs
+++ Program.fs
@@ -1,2 +1,14 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
+
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+open BenchmarkDotNet.Jobs
+
+[<SimpleJob(RuntimeMoniker.Net60)>]
+type Benchmarks() =
+
+    [<Benchmark>]
+    member this.Array() = [| 0..10 |] |> Array.map ((+) 1)
+
+BenchmarkRunner.Run<Benchmarks>() |> ignore
